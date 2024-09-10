9249a5092987ea1ae904f7951f86b9d104e1d98c
unknown <Shakira_leo>
2024-09-09 23:10:53 -0500

    importando librerias

--- python-11.py
+++ python-11.py
@@ -1,2 +1,16 @@
 # empezando proyecto
 # paso 1: hacer mi repositorio
+# importando 
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
