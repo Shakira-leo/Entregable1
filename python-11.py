f0f6fdbd54f23edcdfbe1c3cb9d09f7014c90153
unknown <Shakira_leo>
2024-09-09 23:13:54 -0500

    Crear los datos y guardar el archivo ventas_actualiazadas.csv

--- python-11.py
+++ python-11.py
@@ -14,3 +14,31 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+
+# Paso 1: Crear los datos y guardar el archivo ventas_actualizadas.csv
+data_updated = {
+    'Fecha de Venta': [
+        '2023-01-01', '2023-01-02', '2023-01-03', '2023-01-04', '2023-01-05',
+        '2023-11-08', '2023-11-13', '2023-12-15', '2023-12-12', '2023-12-18',
+        '2024-01-01', '2024-02-03', '2024-02-05', '2024-07-10', '2024-07-15',
+        '2024-08-05', '2024-08-06', '2024-08-07', '2024-09-12', '2024-09-15'
+    ],
+    'Producto': [
+        'Collar', 'Arete', 'Pulsera', 'Reloj', 'Anillo',
+        'Brasalete', 'Llavero', 'Cartera', 'Correa', 'Bolso',   
+        'Perfume', 'Tiara', 'Loción', 'Crema_Corporal', 'Shampoo',
+        'Acondicionador', 'Exfoliante', 'Charms', 'Lentes', 'Ganchos'
+    ],
+    'Cantidad Vendida': [
+        1250, 100, 400, 1, 180,
+        165, 1, 800, 180, 299,
+        185, 535, 1, 1, 95,
+        95, 1, 80, 80, 25
+    ],
+    'Precio Unitario': [
+        30.0, 25.0, 20.0, 50.0, 15.0,
+        40.0, 45.99, 60.0, 20.0, 50.0,
+        55.0, 60.0, 99.90, 99.90, 70.0,
+        75.0, 75.50, 35.0, 45.0, 15.0
+    ]
+}
